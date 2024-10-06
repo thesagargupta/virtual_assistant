5f31dbb6378ff391a314f6ba894a1e449f391a74
sagar gupta <sagarkshn8@gmail.com>
2024-10-06 22:30:36 +0530

    uploading

---
+++ dist/musiclibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "nimbu kharbuja bhail": "https://www.youtube.com/watch?v=dwiXguIajEA",
+    "lollipop": "https://youtu.be/Gr8G_ldltDE?si=gTqxHKy72LKgdw8f",
+    "awdhesh premi song": "https://www.youtube.com/watch?v=yRW1o1cEDiI&list=RDEM-FTA6ih9ouDIMMobHgKajA&start_radio=1",
+    "for suraj" : "https://www.youtube.com/watch?v=LADBU-4X_Ec",
+    "for dharam" : "https://www.youtube.com/watch?v=j4eIxICwOC8",
+    "for sandeep" : "https://www.youtube.com/watch?v=rI478G_qPT0"
+}
--- musiclibrary.py
+++ musiclibrary.py
@@ -4,5 +4,8 @@ music = {
     "awdhesh premi song": "https://www.youtube.com/watch?v=yRW1o1cEDiI&list=RDEM-FTA6ih9ouDIMMobHgKajA&start_radio=1",
     "for suraj" : "https://www.youtube.com/watch?v=LADBU-4X_Ec",
     "for dharam" : "https://www.youtube.com/watch?v=j4eIxICwOC8",
-    "for sandeep" : "https://www.youtube.com/watch?v=rI478G_qPT0"
+    "for sandeep" : "https://www.youtube.com/watch?v=rI478G_qPT0",
+    "for her": "https://www.youtube.com/watch?v=cOM6fXm5yqU",
+    "for me": "https://www.youtube.com/watch?v=XO8wew38VM8",
+    "for sagar": "https://www.youtube.com/watch?v=XO8wew38VM8"
 }
